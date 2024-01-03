2b9370f61856406bcd0b9c15d109c52c1495b911
Jinn <115912358+Jinnlk@users.noreply.github.com>
2024-01-03 00:02:46 -0800

    code cleanup

--- src/App.js
+++ src/App.js
@@ -8,26 +8,6 @@ function Square({ value, onSquareClick }) {
     );
 }
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
-
 export default function Board() {
     const [xIsNext, setxIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
@@ -76,3 +56,24 @@ export default function Board() {
         </>
     );
 }
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
